045862c4ba89a07d57d17cb57109d092a1c1cdbb
unknown <elarrin@gmail.com>
2022-10-24 22:30:56 +0300

    automatic render card - done

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import cardImg from '../../img/cardImg.jpg'
+import privilegeDate from '../../models'
 
-const Card = () => {
+interface cards {
+  card: privilegeDate;
+}
+
+const Card = (props:cards) => {
   return (
     <div className='card'>
       <div className="card__img-container">
         <img className='card__img' src={cardImg} alt="Фоновая картинка" />
       </div>
       <div className="card__content">
-        <p className='card__text card__text_timing'>БЕССРОЧНО</p>
-        <p className='card__text card__text_title'>Сеть бизнес клубов СССР</p>
-        <p className='card__text card__text_descript'>Узнать подробную информацию, кодовое слово, телефон, персонального инструктора</p>
+        <p className='card__text card__text_timing'>{props.card.date_stop}</p>
+        <p className='card__text card__text_title'>{props.card.name}</p>
+        <p className='card__text card__text_descript'>{props.card.description}</p>
       </div>
     </div>
   )
--- src/components/CardContainer/CardContainer.tsx
+++ src/components/CardContainer/CardContainer.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import Card from '../Card/Card';
+import cardData from '../../data/cards'
+import privilegeDate from '../../models'
 
 const CardContainer = () => {
+  const cards = cardData.map((item:privilegeDate) => {
+    return <Card card={item} key={item.id}/>
+  })
   return (
     <div className='card-container'>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
-      <Card/>
+      {cards}
     </div>
   )
 }
